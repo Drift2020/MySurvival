53d149dfcece2e2d6969d7bf09d8ed91d1b8752b
Mamoru40000 <32417036+Drift2020@users.noreply.github.com>
2022-05-10 18:32:33 +0300

    Init
    Add map and player

---
+++ Source/MySurvival/MySurvivalGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MySurvivalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYSURVIVAL_API AMySurvivalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
