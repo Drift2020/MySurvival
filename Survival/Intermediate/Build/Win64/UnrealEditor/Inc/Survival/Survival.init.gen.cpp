06d6ad1e78999509d2fa1098d9667693f9316c05
Mamoru40000 <32417036+Drift2020@users.noreply.github.com>
2022-05-11 14:02:00 +0300

    1

---
+++ Survival/Intermediate/Build/Win64/UnrealEditor/Inc/Survival/Survival.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeSurvival_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_Survival;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_Survival()
+	{
+		if (!Z_Registration_Info_UPackage__Script_Survival.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/Survival",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0xF8474897,
+				0x8ECE8D4D,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Survival.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_Survival.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Survival(Z_Construct_UPackage__Script_Survival, TEXT("/Script/Survival"), Z_Registration_Info_UPackage__Script_Survival, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xF8474897, 0x8ECE8D4D));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Survival/Intermediate/Build/Win64/UnrealEditor/Inc/Survival/SurvivalGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SURVIVAL_SurvivalGameModeBase_generated_h
+#error "SurvivalGameModeBase.generated.h already included, missing '#pragma once' in SurvivalGameModeBase.h"
+#endif
+#define SURVIVAL_SurvivalGameModeBase_generated_h
+
+#define FID_Survival_Source_Survival_SurvivalGameModeBase_h_15_SPARSE_DATA
+#define FID_Survival_Source_Survival_SurvivalGameModeBase_h_15_RPC_WRAPPERS
+#define FID_Survival_Source_Survival_SurvivalGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Survival_Source_Survival_SurvivalGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesASurvivalGameModeBase(); \
+	friend struct Z_Construct_UClass_ASurvivalGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ASurvivalGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Survival"), NO_API) \
+	DECLARE_SERIALIZER(ASurvivalGameModeBase)
+
+
+#define FID_Survival_Source_Survival_SurvivalGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesASurvivalGameModeBase(); \
+	friend struct Z_Construct_UClass_ASurvivalGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ASurvivalGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Survival"), NO_API) \
+	DECLARE_SERIALIZER(ASurvivalGameModeBase)
+
+
+#define FID_Survival_Source_Survival_SurvivalGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASurvivalGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASurvivalGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASurvivalGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASurvivalGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASurvivalGameModeBase(ASurvivalGameModeBase&&); \
+	NO_API ASurvivalGameModeBase(const ASurvivalGameModeBase&); \
+public:
+
+
+#define FID_Survival_Source_Survival_SurvivalGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASurvivalGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASurvivalGameModeBase(ASurvivalGameModeBase&&); \
+	NO_API ASurvivalGameModeBase(const ASurvivalGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASurvivalGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASurvivalGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASurvivalGameModeBase)
+
+
+#define FID_Survival_Source_Survival_SurvivalGameModeBase_h_12_PROLOG
+#define FID_Survival_Source_Survival_SurvivalGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Survival_Source_Survival_SurvivalGameModeBase_h_15_SPARSE_DATA \
+	FID_Survival_Source_Survival_SurvivalGameModeBase_h_15_RPC_WRAPPERS \
+	FID_Survival_Source_Survival_SurvivalGameModeBase_h_15_INCLASS \
+	FID_Survival_Source_Survival_SurvivalGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_Survival_Source_Survival_SurvivalGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Survival_Source_Survival_SurvivalGameModeBase_h_15_SPARSE_DATA \
+	FID_Survival_Source_Survival_SurvivalGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Survival_Source_Survival_SurvivalGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	FID_Survival_Source_Survival_SurvivalGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SURVIVAL_API UClass* StaticClass<class ASurvivalGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Survival_Source_Survival_SurvivalGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
